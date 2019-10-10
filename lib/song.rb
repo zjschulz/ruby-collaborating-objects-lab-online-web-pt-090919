9dae34be754ec247503c12c0629d4d726e117cbf
Zachary Schulz <zjschulz@gmail.com>
2019-10-10 01:23:28 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song 
+  
+  attr_accessor :artist, :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
